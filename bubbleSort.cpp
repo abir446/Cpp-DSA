ce199216506902f1d1dd5bebd23e05f3f153de87
abir446 <108267299+abir446@users.noreply.github.com>
2023-07-13 09:40:08 +0530

    Optimized bubbleSort.cpp

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,27 +1,27 @@
-#include <any>
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-void bubbleSort(int arr[],int n){
-
-    for (int i=0;i<n-1;i++)
-    {
-        for (int j=0;j<n-i-1;j++)
+vector<int> optimizedBubbleSort(vector<int> arr){
+    // your code  goes here
+    int i, j, n=arr.size();
+   bool swapped;
+   for (i = 0; i < n-1; i++)
+   {
+     swapped = false;
+     for (j = 0; j < n-i-1; j++)
+     {
+        if (arr[j] > arr[j+1])
         {
-            if (arr[j]>arr[j+1])
-                swap(arr[j], arr[j+1]);
+           swap(arr[j], arr[j+1]);
+           swapped = true;
         }
-    }
-}
-
-int main()
-{
-    int arr[] = {-2,3,4,-1,5,-12,6,1,3};
-    int n=sizeof(arr)/sizeof(int);
-    bubbleSort(arr, n);
-    for (auto x: arr)
-    {
-        cout<<x<<" ";
-    }
-    return 0;
+     }
+ 
+     // IF no two elements were swapped by inner loop, then break
+     if (swapped == false)
+        break;
+   }
+   
+   return arr;
+    
 }
